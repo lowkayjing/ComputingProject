6639e7265d8d82106e86711c12c63b5690e1e8bd
lowkayjing <kayjing.low@sd.taylors.edu.my>
2021-12-01 10:25:51 +0800

    remove footer-box background

--- check_login.php
+++ check_login.php
@@ -16,7 +16,9 @@ if(!empty($_POST["username"]) && !empty($_POST["password"]))
     $database = "bid4u_system_db";
 
     $conn = new mysqli( $servername, $username, $password, $database);
-    
+
+
+
     if($conn->connect_error)
     {
         die("Connection failed!".$conn->connect_error);

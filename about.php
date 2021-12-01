6b562c7bb0e2f0c41db6e3cbc87304efb3b5f341
ZM030508 <85633012+ZM030508@users.noreply.github.com>
2021-12-01 10:22:48 +0800

    Update about.php

--- about.php
+++ about.php
@@ -1,11 +1,10 @@
 <html>
-<head><title></title>
+<head><title>Auction System</title>
 
 
     <link rel="stylesheet" type="text/css" href="css/display.css">
     
 </head>
-
 <body>
 <script src="JS/display.js"> </script>
 <header>
@@ -32,24 +31,61 @@
                 <div class="modal-header">
                 </div>
                 <div class="modal-body">
-
                     <h3><i class="glyphicon glyphicon-thumbs-up"></i>The purpose of this project is to establish an "Online Auction System", a store for buyers and stores to be contacted almost anywhere.<br><br><i class="glyphicon glyphicon-thumbs-up"></i>Users do not need to register to view the functions of this website, but users who can bid and sell must register.<br><br>
-                        <i class="glyphicon glyphicon-thumbs-up"></i>The auction has a name, a description, possibly a photo uploaded by the user (of the related item), and an end time: when the auction interval ends, the user cannot bid, but if there is no item for which the offer is offered, it may be suspended.<br><br> <i class="glyphicon glyphicon-thumbs-up"></i>In addition, the administrator can accept or reject auctions proposed by users, view information about users and items, and create, modify, and delete auction categories (for mobile phones, and computers).</h3>
-
+                        <i class="glyphicon glyphicon-thumbs-up"></i>The auction has a name, a description, possibly a photo uploaded by the user (of the related item), and an end time: when the auction interval ends, the user cannot bid, but if there is no item for which the offer is offered, it may be suspended.<br>
+                        <br> <i class="glyphicon glyphicon-thumbs-up"></i>In addition, the administrator can accept or reject auctions proposed by users, view information about users and items, and create, modify, and delete auction categories (for mobile phones, and computers).</h3>
                 </div>
             </div>
         </div>
     </div>
-    <!--Begin : Page FOOTER -->
-    <div class="footer_bottom">
+
+
+    <footer class="footer-box">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="full">
+                <div class="heading_main text_align_center white_fonts margin-bottom_30">
+                
+                    </div>
+                </div>
+            </div>
+        </div>
+
+            <div class="col-lg-3 col-md-6 col-sm-6 white_fonts">
+                
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 col-sm-6 white_fonts">
+            
+        </div>
+    </div>
+
+    <div class="col-lg-3 col-md-6 col-sm-6 white_fonts">
+    </div>
+    </div>
+    </div>
+    </div>
+</footer>
+<!--Begin : Page FOOTER -->
+<div class="footer_bottom">
         <div class="container">
             <div class="row">
                 <div class="col-12">
                     <p class="crp">Copyright 2021 © Bid4u.Com</p>
+                    <h3>Contact Us</h3>
+                    <p> Address <br/>
+    <small>1, Jalan Taylors,<br/>
+    47500 Subang Jaya, Selangor.<br/>
+    </small></p>
+    <p>Phone<br><small>03-5629 5000<br>Monday - Friday<br>8:00 am - 6:00 pm</small></p>
+    <p>Email<br><small>Bid4u@outlook.com</small></p>
                 </div>
             </div>
         </div>
-    </div>
-    <!--End : Page FOOTER -->
+</div>
+<!--End : Page FOOTER -->
+
 </body>
 </html>

b5d689acc23a5aa841d63a03f69371e333bd2650
ZM030508 <85633012+ZM030508@users.noreply.github.com>
2021-12-01 10:25:29 +0800

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,6 +1,6 @@
 <html>
 <head><title>Auction System</title>
-    <link rel="stylesheet" type="text/css" href="css/contact.css">
+    <link rel="stylesheet" type="text/css" href="css/display.css">
    
 </head>
 <body>
@@ -38,12 +38,12 @@
                         <br>
                         <input class='form-control' type='text' name='name' placeholder='Enter your name'required/>
                         <br>
-
+                        <br>
                         <label for='exampleInputEmail1' class='label'> Email address: </label>
                         <br>
                         <input class='form-control' type='email' name='email' placeholder='Enter your Email'required/>
                         <br>
-
+                        <br>
                         <label for='Meggase' class='label'> Text: </label>
                         <br>
                         <textarea rows="3" cols="68" type='text' name='message' placeholder='Enter your Comment(s)'></textarea>
@@ -57,16 +57,52 @@
                 </div>
             </div>
         </div>
-        <!--Begin : Page FOOTER -->
-        <div class="footer_bottom">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12">
-                        <p class="crp">Copyright 2021 © Bid4u.Com</p>
+        <footer class="footer-box">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="full">
+                <div class="heading_main text_align_center white_fonts margin-bottom_30">
+                
                     </div>
                 </div>
             </div>
         </div>
-        <!--End : Page FOOTER -->
+
+            <div class="col-lg-3 col-md-6 col-sm-6 white_fonts">
+                
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 col-sm-6 white_fonts">
+            
+        </div>
+    </div>
+
+    <div class="col-lg-3 col-md-6 col-sm-6 white_fonts">
+    </div>
+    </div>
+    </div>
+    </div>
+</footer>
+<!--Begin : Page FOOTER -->
+<div class="footer_bottom">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <p class="crp">Copyright 2021 © Bid4u.Com</p>
+                    <h3>Contact Us</h3>
+                    <p> Address <br/>
+    <small>1, Jalan Taylors,<br/>
+    47500 Subang Jaya, Selangor.<br/>
+    </small></p>
+    <p>Phone<br><small>03-5629 5000<br>Monday - Friday<br>8:00 am - 6:00 pm</small></p>
+    <p>Email<br><small>Bid4u@outlook.com</small></p>
+                </div>
+            </div>
+        </div>
+</div>
+<!--End : Page FOOTER -->
+
     </body>
 </html>
